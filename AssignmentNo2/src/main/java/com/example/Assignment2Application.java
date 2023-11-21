747869ab1b7b6ea8a4c379a228d23769b6c0bc4e
karanpawar0820 <karanpawar0820@gmail.com>
2023-11-21 20:56:27 +0530

    1st Commit A2

---
+++ AssignmentNo2/src/main/java/com/example/Assignment2Application.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages="com.example")
+public class Assignment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment2Application.class, args);
+	}
+
+}
